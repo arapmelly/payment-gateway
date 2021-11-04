664a4d09cd54734d2e437fa09ab4101a8e1bd7dd
George <kariukigeorge511@gmail.com>
2021-11-04 18:51:07 +0300

    Paypal integration

---
+++ config/paypal.php
@@ -0,0 +1,12 @@
+<?php 
+return [ 
+    'client_id' => 'ASOIl1nQdTjiPb8nb_uqt1kHW_eTVt1vQMRcr2ONXt5d1jwPtP9lVQCzcGApan5GrymtLK4084i76QeC',
+	'secret' => 'EMaKMmxlMuG4AxB50qlpp8-fiUljAD4QOw6lzp9rN-Zboct9zw4nmlQBR8ntvvWOqzpeGfzu-Hx4jEak',
+    'settings' => array(
+        'mode' => 'sandbox',
+        'http.ConnectionTimeOut' => 1000,
+        'log.LogEnabled' => true,
+        'log.FileName' => storage_path() . '/logs/paypal.log',
+        'log.LogLevel' => 'FINE'
+    ),
+];
